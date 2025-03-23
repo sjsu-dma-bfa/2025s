286c0425e3e28d518a26ecb0a5aa5bd15d965b98
BenBeary <120347482+BenBeary@users.noreply.github.com>
2025-03-22 23:04:35 -0700

    Removing Json loading System because it wont work with github build

--- js/ArtistPageFiller.js
+++ js/ArtistPageFiller.js
@@ -17,9 +17,6 @@ leftButton.addEventListener("click", function () {
         top: 0,
         behavior: 'smooth' // Adds smooth scrolling effect
     });
-    setTimeout(function() {
-        fetchPeopleData();
-    }, 1000);
 });
 rightButton.addEventListener("click", function () {
     index += 1; 
@@ -28,101 +25,26 @@ rightButton.addEventListener("click", function () {
         top: 0,
         behavior: 'smooth' // Adds smooth scrolling effect
     });
-    setTimeout(function() {
-        fetchPeopleData();
-    }, 1000);
 })
 
 
 
+const newImage = document.getElementById("artwork")
 
 
-const params = new URLSearchParams(window.location.search);
+function updatePage(){
 
-if (params.has('artist')) {
-    artist = params.get('artist');
-}
-if(params.has("index")){
-    index = params.get("index");
-}
+    console.log(newImage.naturalHeight + " x " + newImage.naturalWidth)
 
-function fetchPeopleData(){
-    fetch("../json/test.json")
-        .then(repsonse => {
-            if(!repsonse.ok) {
-                throw new Error("Failed to get response");
-            }
-
-            return repsonse.json();
-        })
-        .then(data => {
-            updatePage(data);
-        })
-}
-
-function updatePage(data){
-
-    if(index >= data.Artist.length){
-        index = 0;
+    if(newImage.naturalHeight >= newImage.naturalWidth) {
+        newImage.classList.add("tall");
     }
-    else if(index < 0){
-        index = data.Artist.length-1;
+    else{
+        newImage.classList.add("wide");
     }
-
-    if(document.getElementById("artImage") !== null && document.getElementById("artImage").id !== ""){
-            
-        newImage = document.getElementById("artImage").remove();
-    }
-
-    // Project Title
-    document.getElementById('title').innerHTML = data.Artist[index].title;
-
-    // Artist Name
-    document.getElementById('name').innerHTML = data.Artist[index].name;
-
-    document.getElementById('profile_pic').src = "../images/" + data.Artist[index].profilePicture;
-
-    // Artist Statement
-    document.getElementById('statement').innerHTML = data.Artist[index].statement;
-
-    // Artist Biography
-    document.getElementById('bio').innerHTML = data.Artist[index].biography;
-
-    // Artist Project Display
-    const imgFile = "../images/" + String(data.Artist[index].display);
-
-    //document.getElementById("artkwork").innerHTML += '<img src="/images/light-mascot-1.png" alt="testing">';
-
-    if(imgFile.toLocaleLowerCase().endsWith('.png') || imgFile.toLocaleLowerCase().endsWith('.jpg') || imgFile.toLocaleLowerCase().endsWith('.jpeg') ){
-        
-
-
 
     
-        const newImage = new Image()
-        newImage.src = imgFile;
-        newImage.classList.add("artwork");
-        newImage.id = "artImage"
-        document.getElementById("artwork").appendChild(newImage);
-    
-
-        
-        newImage.onload = function() {
-            const width = newImage.naturalWidth;
-            const height = newImage.naturalHeight;
-
-            if (height >= width) {
-                console.log('The image is Tall or Square.');
-                newImage.classList.add("tall");
-            } 
-            else {
-                console.log('The image is Wide');
-                newImage.classList.add("wide");
-            }
-        }
-
-    }
     
 }
 
-fetchPeopleData();
+updatePage()
