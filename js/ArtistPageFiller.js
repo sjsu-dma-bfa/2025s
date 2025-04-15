7fdb216595af8aa09dadace5a27ea4d1a53e443b
BenBeary <120347482+BenBeary@users.noreply.github.com>
2025-04-14 19:26:02 -0700

    Fixes to the Artist Template

--- js/ArtistPageFiller.js
+++ js/ArtistPageFiller.js
@@ -27,24 +27,37 @@ rightButton.addEventListener("click", function () {
     });
 })
 
+// ------------------------------
+// Modal Stuff
+// ------------------------------
+
+const modal = document.getElementById("imageModal"); // the container
+const modalImg = document.getElementById("modalImg"); // the display Image
+const closeBtn = document.getElementById("closeModal"); // a button to close container
+const thumbnails = document.querySelectorAll(".wide, .tall"); // all images allowed to be in modal
+
+// Code for adding selected image to modal
+thumbnails.forEach(img => {
+    img.addEventListener("click", () => {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+      modalImg.alt = img.alt;
+    });
+  });
+
+  // Hides modal window
+  closeBtn.onclick = () => {
+    modal.style.display = "none";
+  };
 
 
-const newImage = document.getElementById("artwork")
-
-
-function updatePage(){
-
-    console.log(newImage.naturalHeight + " x " + newImage.naturalWidth)
-
-    if(newImage.naturalHeight >= newImage.naturalWidth) {
-        newImage.classList.add("tall");
+  // This hides the modal if you click outside of the image
+  modal.onclick = (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
     }
-    else{
-        newImage.classList.add("wide");
-    }
+  };
 
-    
-    
-}
 
-updatePage()
+
+
